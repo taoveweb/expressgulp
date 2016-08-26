35aba01e6bcfbeabbc6cb35feafc23045d31dc2b
taoveweb <taoveweb@gmail.com>
2016-08-26 17:13:43 +0800

    all

--- app/controllers/admin/admin.js
+++ app/controllers/admin/admin.js
@@ -10,25 +10,36 @@ module.exports = function (app) {
   app.use('/admin', auth.adminLogin, router);
 };
 
+//会员列表
 router.get('/', function (req, res, next) {
   co(function *() {
-    var pageSize = 14;
+    var pageSize = 20;
     var count = yield User.count({});
     var pageNum = req.query.page || 1;
-    var pageCount=Math.ceil(count/pageSize);
-    var starPage=pageNum-7<0 ? 0 :pageNum-7;
-    var endPage=starPage+14>pageCount ? pageCount :starPage+14;
-    var users = yield User.find().skip((pageNum-1)*pageSize).limit(pageSize);
-    if(pageNum>pageCount){
-      pageNum=pageCount
+    if (pageNum > pageCount) {
+      pageNum = pageCount
     }
+    var pageCount = Math.ceil(count / pageSize);
+    var starPage = 1;
+    if(pageNum>pageSize ){
+      starPage=Math.floor(pageNum / pageSize) * pageSize+1
+    }
+
+    var endPage = starPage + pageSize >= pageCount ? pageCount+1 : starPage + pageSize;
+    var prePage = starPage - pageSize <= 0 ? 1 : starPage - pageSize;
+    var nextPage = starPage + pageSize >= pageCount ? pageCount : starPage + pageSize;
+    var users = yield User.find().skip((pageNum - 1) * pageSize).limit(pageSize);
+
     res.render('admin/userlist', {
       title: '会员列表',
       users: users,
-      pageNum:pageNum,
-      pageCount:pageCount,
-      starPage:starPage,
-      endPage:endPage
+      pageNum: pageNum,
+      pageCount: pageCount,
+      starPage: starPage,
+      endPage: endPage,
+      prePage: prePage,
+      nextPage: nextPage,
+      currentPage:parseInt(pageNum)
     });
   }).catch(function (err) {
     console.log('get /admin', err)

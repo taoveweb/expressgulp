29035b1edc2174f092b951da54401d717860d4d2
taoveweb <taoveweb@gmail.com>
2016-08-26 18:02:57 +0800

    all

--- app/controllers/admin/admin.js
+++ app/controllers/admin/admin.js
@@ -21,6 +21,16 @@ router.get('/', function (req, res, next) {
     }
     var pageCount = Math.ceil(count / pageSize);
     var starPage = 1;
+    var sort={created:1};
+    if(req.query.fans){
+      sort={fans:parseInt(req.query.fans)}
+    }
+    if(req.query.created){
+      sort={created:parseInt(req.query.created)}
+    }
+    if(req.query.concern){
+      sort={concern:parseInt(req.query.concern)}
+    }
     if(pageNum>pageSize ){
       starPage=Math.floor(pageNum / pageSize) * pageSize+1
     }
@@ -28,7 +38,7 @@ router.get('/', function (req, res, next) {
     var endPage = starPage + pageSize >= pageCount ? pageCount+1 : starPage + pageSize;
     var prePage = starPage - pageSize <= 0 ? 1 : starPage - pageSize;
     var nextPage = starPage + pageSize >= pageCount ? pageCount : starPage + pageSize;
-    var users = yield User.find().skip((pageNum - 1) * pageSize).limit(pageSize);
+    var users = yield User.find().sort(sort).skip((pageNum - 1) * pageSize).limit(pageSize);
 
     res.render('admin/userlist', {
       title: '会员列表',

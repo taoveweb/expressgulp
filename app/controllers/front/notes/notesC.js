4c6d55fe1cd9b9a8aa4c61bef039bfc817dac39a
taoveweb <taoveweb@gmail.com>
2016-09-22 12:39:12 +0800

    all route

--- app/controllers/front/notes/notesC.js
+++ app/controllers/front/notes/notesC.js
@@ -13,10 +13,12 @@ router.get('/comments' , function (req, res, next) {
     res.render('mobile/notes/comments', {
       layout:"main_m",
       title: '我收到的评论 - 偶酷网 - 最好的摄影师都在这',
+      route:"comments",
     });
   }else {
     res.render('pc/notes/comments', {
       title: '我收到的评论 - 偶酷网 - 最好的摄影师都在这',
+      route:"comments",
     });
   }
 
@@ -28,10 +30,12 @@ router.get('/likes' , function (req, res, next) {
     res.render('mobile/notes/likes', {
       layout:"main_m",
       title: '我被赞同的评论 - 偶酷网 - 最好的摄影师都在这',
+      route:"likes",
     });
   }else {
     res.render('pc/notes/likes', {
       title: '我收到的评论 - 偶酷网 - 最好的摄影师都在这',
+      route:"likes",
     });
   }
 
@@ -41,10 +45,12 @@ router.get('/favorites' , function (req, res, next) {
     res.render('mobile/notes/favorites', {
       layout:"main_m",
       title: '我的分享 - 偶酷网 - 最好的摄影师都在这',
+      route:"favorites",
     });
   }else {
     res.render('pc/notes/favorites', {
       title: '我的分享 - 偶酷网 - 最好的摄影师都在这',
+      route:"favorites",
     });
   }
 });
@@ -53,10 +59,12 @@ router.get('/my-comments' , function (req, res, next) {
     res.render('mobile/notes/my-comments', {
       layout:"main_m",
       title: '我发表的评论 - 偶酷网 - 最好的摄影师都在这',
+      route:"my-comments",
     });
   }else {
     res.render('pc/notes/my-comments', {
       title: '我发表的评论 - 偶酷网 - 最好的摄影师都在这',
+      route:"my-comments",
     });
   }
 });
@@ -65,10 +73,12 @@ router.get('/my-likes' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/notes/my-likes', {
       layout:"main_m",
+      route:"my-likes",
       title: '我赞同的评论 - 偶酷网 - 最好的摄影师都在这',
     });
   }else {
     res.render('pc/notes/my-likes', {
+      route:"my-likes",
       title: '我赞同的评论 - 偶酷网 - 最好的摄影师都在这',
     });
   }
--- app/controllers/front/personal/personalC.js
+++ app/controllers/front/personal/personalC.js
@@ -4,7 +4,6 @@ var express = require('express'),
   User = mongoose.model('User');
 
 module.exports = function (app) {
-
   app.use('/1551288', router);
 };
 
@@ -13,11 +12,13 @@ router.get('/' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/personal/personalMain', {
       title: '子一 - 偶酷摄影网',
+      route:'home',
       layout:'main_m'
     });
   }else {
     res.render('pc/personal/personalMain', {
       title: '子一 - 偶酷摄影网',
+      route:'home',
       layout:'main'
     });
   }
@@ -30,11 +31,13 @@ router.get('/albums' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/personal/albums', {
       title: '我的相册 - 子一824 - 偶酷摄影网',
+      route:'albums',
       layout:'main_m'
     });
   }else {
     res.render('pc/personal/albums', {
       title: '我的相册 - 子一824 - 偶酷摄影网',
+      route:'albums',
       layout:'main'
     });
   }
@@ -47,11 +50,13 @@ router.get('/following' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/personal/following', {
       title: '个人主页- 偶酷摄影网',
+      route:'following',
       layout:'main_m'
     });
   }else {
     res.render('pc/personal/following', {
       title: '个人主页- 偶酷摄影网',
+      route:'following',
       layout:'main'
     });
   }
@@ -62,11 +67,13 @@ router.get('/favorites' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/personal/favorites', {
       title: '子一824的分享 - 子一824 - 偶酷摄影网',
+      route:'favorites',
       layout:'main_m'
     });
   }else {
     res.render('pc/personal/favorites', {
       title: '子一824的分享 - 子一824 - 偶酷摄影网',
+      route:'favorites',
       layout:'main'
     });
   }
@@ -78,11 +85,13 @@ router.get('/collections' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/personal/collections', {
       title: '子一824的收藏夹 - 偶酷摄影网',
+      route:'collections',
       layout:'main_m'
     });
   }else {
     res.render('pc/personal/collections', {
       title: '子一824的收藏夹- 偶酷摄影网',
+      route:'collections',
       layout:'main'
     });
   }
@@ -93,11 +102,13 @@ router.get('/profile' , function (req, res, next) {
   if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
     res.render('mobile/personal/profile', {
       title: '我的资料 - 子一824 - 偶酷摄影网',
+      route:'profile',
       layout:'main_m'
     });
   }else {
     res.render('pc/personal/profile', {
       title: '我的资料 - 子一824 - 偶酷摄影网',
+      route:'profile',
       layout:'main'
     });
   }

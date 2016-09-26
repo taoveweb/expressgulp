656b201dbbd5ea8f86b555173e34809f47b51522
taoveweb <taoveweb@gmail.com>
2016-09-26 14:01:17 +0800

    all

--- app/controllers/front/signup/signupC.js
+++ app/controllers/front/signup/signupC.js
@@ -5,27 +5,19 @@ var express = require('express'),
   auth = require('../user/user');
 
 module.exports = function (app) {
-  app.use('/', router);
+  app.use('/signup', router);
 };
 
 router.get('/', function (req, res, next) {
-  console.log(req.user)
   if (req.headers["user-agent"].toLowerCase().indexOf('mobile') !== -1) {
-    res.render('mobile/home/homeMain', {
+    res.render('mobile/signup/signup', {
       layout: "main_m",
-      title: '首页 - 偶酷网 - 最好的摄影师都在这',
+      title: '注册帐号 - 偶酷网 - 最好的摄影师都在这',
       route: "动态",
     });
   } else {
-    var template;
-    if (req.user) {
-      template="pc/home/homeMain";
-    } else {
-      template="pc/home/homeNoLogin";
-    }
-
-    res.render(template, {
-      title: '首页 - 偶酷网 - 最好的摄影师都在这',
+    res.render('pc/signup/signup', {
+      title: '注册帐号 - 偶酷网 - 最好的摄影师都在这',
       route: "动态",
     });
   }

7aa303774ca88dd223d99eccee8cefc21f1f99d1
黄 家金 <hjj245148>
2016-10-01 19:09:08 +0800

    all

--- public/pc/js/jupload.js
+++ public/pc/js/jupload.js
@@ -1,7 +1,10 @@
 /**
  * Created by Administrator on 2016/9/30 0030.
  */
+
+
 $(function () {
+  //上传图片
   $('#file_upload').fileupload({
     acceptFileTypes: /(\.|\/)(jpe?g|png)$/i,
     imageMinWidth:600,
@@ -28,4 +31,35 @@ $(function () {
     .bind('fileuploadprocessstop', function (e) {
       console.log(fileuploadprocessstop)
     });
+
+
+  //新建相册
+  $('.popCreateAlbum').click(function(){
+    layer.open({
+      type: 1, //page层
+      area: ['500px', '213'],
+      title: '创建新相册',
+      shade: 0.6, //遮罩透明度
+      moveType: 1, //拖拽风格，0是默认，1是传统拖动
+      shift: -1, //0-6的动画形式，-1不开启
+      content: $('.form-horizontal')
+    });
+  })
+
+  $('.resetalbums').click(function(){
+    layer.closeAll()
+  })
+
+  $('.submitalbums').click(function(){
+
+    var params = $('.form-horizontal').serializeArray();
+    var data = {};
+    params.map(function (val, i) {
+      data[val.name] = val.value;
+    });
+    $.ajax('/rest/albums',data,function(response){
+        console.log(response)
+    })
+  })
+
 });

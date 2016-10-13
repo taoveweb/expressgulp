84e86374b41a36437d1e13c70ed7373683e0352e
taoveweb <taoveweb@gmail.com>
2016-10-13 11:37:32 +0800

    all

--- app/controllers/front/groups/groups.js
+++ app/controllers/front/groups/groups.js
@@ -33,6 +33,21 @@ router.get('/all' , function (req, res, next) {
 
 });
 
+router.get('/new' , function (req, res, next) {
+  if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
+    res.render('mobile/groups/new', {
+      layout:"main_m",
+      title: '申请小组 - 偶酷网 - 最好的摄影师都在这',
+    });
+  }else {
+    res.render('pc/groups/new', {
+      title: '申请小组 - 偶酷网 - 最好的摄影师都在这',
+    });
+  }
+
+});
+
+
 
 
 

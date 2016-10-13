3aec4012513a8476272fc2f1dc15126bc296a184
taoveweb <taoveweb@gmail.com>
2016-10-13 11:58:58 +0800

    'all'

--- app/controllers/front/new-text/new-textC.js
+++ app/controllers/front/new-text/new-textC.js
@@ -1,20 +1,21 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
+  auth = require('../user/user'),
   User = mongoose.model('User');
 
 module.exports = function (app) {
-  app.use('/new-text',auth.requireLogin, router);
+  app.use('/new-text', auth.requireLogin, router);
 };
 
 router.get('/', function (req, res, next) {
 
-  if(req.headers["user-agent"].toLowerCase().indexOf('mobile')!==-1){
+  if (req.headers["user-agent"].toLowerCase().indexOf('mobile') !== -1) {
     res.render('mobile/new-text/new-text', {
-      layout:"main_m",
+      layout: "main_m",
       title: '发表文字 - 偶酷网 - 最好的摄影师都在这',
     });
-  }else {
+  } else {
     res.render('pc/new-text/new-text', {
       title: '发表文字 - 偶酷网 - 最好的摄影师都在这',
     });
